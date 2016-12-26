758a41e6797b194ce3804d5008b5072d6784fa6b
Darin Minamoto <darinm223@gmail.com>
2016-12-25 23:50:08 -0800

    Started manga scraping skeleton

--- utils/scraper.js
+++ utils/scraper.js
@@ -1,9 +1,39 @@
 import fetch from 'node-fetch'
 
-export function scrape (url, parseBody) {
-  return fetch(url).then(function (res) {
-    return res.text()
-  }).then(function (body) {
-    return parseBody(body)
-  })
+/**
+ * Downloads a specific page of the manga.
+ * @param {string} mangaName
+ * @param {number} chapterNum
+ * @param {number} pageNum
+ * @param {Adapter} adapter
+ * @return {ImageData} // TODO(DarinM223): figure out what fetch(imageURL) returns.
+ */
+export function downloadPage (mangaName, chapterNum, pageNum, adapter) {
+  return fetch(adapter.mangaURL(mangaName, chapterNum, pageNum))
+    .then((res) => res.text())
+    .then((body) => Promise.resolve(adapter.parsePageImage(body)))
+    .then((imageURL) => fetch(imageURL))
+}
+
+/**
+ * Downloads a specific chapter for the manga.
+ */
+export function downloadChapter (mangaName, chapter, adapter) {
+  let promises = []
+  for (const page of chapter.pages) {
+    promises.push(downloadPage(mangaName, chapter.num, page.num, adapter))
+  }
+
+  return Promise.all(promises)
+}
+
+/**
+ * Returns the data for the manga given the manga name.
+ * @param {string} mangaName
+ * @param {Adapter} adapter
+ */
+export function scrape (mangaName, adapter) {
+  return fetch(adapter.mangaURL(mangaName))
+    .then((res) => res.text())
+    .then((body) => adapter.parseBody(body))
 }
--- utils/sites/mangareader.js
+++ utils/sites/mangareader.js
@@ -1,3 +1,43 @@
-export default function parseBody (body) {
-  // TODO(DarinM223): parse the body for a mangareader chapter.
+import cheerio from 'cheerio'
+
+/**
+ * Returns the URL for the given manga. If chapterNum or pageNum is also
+ * specified it returns the URL for the specific chapter or page of the manga.
+ * @param {string} mangaName
+ * @param {number|null} chapterNum
+ * @param {number|null} pageNum
+ * @return {string} the requested URL.
+ */
+export function mangaURL (mangaName, chapterNum = null, pageNum = null) {
+  if (chapterNum !== null && pageNum !== null) {
+    return `http://www.mangareader.net/${mangaName}`
+  } else if (chapterNum !== null) {
+    return `http://www.mangareader.net/${mangaName}/${chapterNum}`
+  } else {
+    return `http://www.mangareader.net/${mangaName}/${chapterNum}/${pageNum}`
+  }
+}
+
+/**
+ * Parses the html body and returns the general manga data like
+ * the dates when chapters came out or the manga name.
+ * @param {string} body
+ * @return {object} the manga as a nested object.
+ */
+export function parseMangaData (body) {
+  const $ = cheerio.load(body)
+
+  // TODO(DarinM223): parse the body for the manga image.
+  // TODO(DarinM223): parse the body for the manga chapters.
+}
+
+/**
+ * Parses the html body and returns the URL of the image for the page of manga.
+ * @param {string} body
+ * @return {string} the image URL of the page.
+ */
+export function parsePageImage (body) {
+  const $ = cheerio.load(body)
+
+  // TODO(DarinM223): parse the body for the image URL.
 }
