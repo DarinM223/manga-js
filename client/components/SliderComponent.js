6748f6d2a12e34e3fed345667616a508811157ec
Darin Minamoto <darinm223@gmail.com>
2017-03-10 04:53:19 -0800

    Page number can be edited directly with input

--- client/components/SliderComponent.js
+++ client/components/SliderComponent.js
@@ -1,5 +1,7 @@
 import React, { PropTypes } from 'react'
 import Slider from 'material-ui/Slider'
+import Chip from 'material-ui/Chip'
+import TextField from 'material-ui/TextField'
 
 const styles = {
   slider: {
@@ -7,26 +9,92 @@ const styles = {
     flexDirection: 'row',
     justifyContent: 'space-around'
   },
-  sliderText: {
+  pageNumberDisplay: {
     position: 'relative',
-    top: '5px'
+    top: '18px',
+    height: '30px'
+  },
+  pageNumberEditing: {
+    display: 'flex',
+    width: '10%',
+    flexDirection: 'row',
+    justifyContent: 'space-around'
   }
 }
 
-export default function SliderComponent ({ currValue, totalPages, onSliderChanged }) {
-  return (
-    <div style={styles.slider}>
-      <Slider
-        style={{ width: '80%' }}
-        min={1}
-        max={totalPages}
-        step={1}
-        value={currValue}
-        onChange={(event, newValue) => onSliderChanged(newValue)}
-      />
-      <p style={styles.sliderText}>{currValue}/{totalPages}</p>
-    </div>
-  )
+export default class SliderComponent extends React.Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      editing: false,
+      pageText: ''
+    }
+  }
+
+  render () {
+    const editPages = () => this.setState({ editing: true, pageText: this.props.currValue + '' })
+    const pageTextChanged = (event, newValue) => {
+      const num = parseInt(newValue, 10)
+      if (newValue.length === 0 || (!isNaN(num) && num <= this.props.totalPages && num > 0)) {
+        this.setState({ pageText: newValue })
+      }
+    }
+    const savePageText = () => {
+      if (this.state.pageText.length === 0) {
+        this.setState({ pageText: this.props.currValue })
+      } else {
+        this.props.onSliderChanged(parseInt(this.state.pageText, 10))
+      }
+
+      this.setState({ editing: false })
+    }
+    const pageTextKeyPress = (event) => {
+      if (event.key === 'Enter') {
+        savePageText()
+      }
+    }
+    const pageLabel = `${this.props.currValue}/${this.props.totalPages}`
+
+    let currPageComponent = null
+    if (this.state.editing) {
+      currPageComponent = (
+        <div style={styles.pageNumberEditing}>
+          <TextField
+            value={this.state.pageText}
+            onChange={pageTextChanged}
+            onBlur={savePageText}
+            onKeyPress={pageTextKeyPress}
+            autoFocus
+          />
+          <p>/{this.props.totalPages}</p>
+        </div>
+      )
+    } else {
+      currPageComponent = (
+        <Chip
+          style={styles.pageNumberDisplay}
+          onTouchTap={editPages}
+        >
+          {pageLabel}
+        </Chip>
+      )
+    }
+
+    return (
+      <div style={styles.slider}>
+        <Slider
+          style={{ width: '80%' }}
+          min={1}
+          max={this.props.totalPages}
+          step={1}
+          value={this.props.currValue}
+          onChange={(event, newValue) => this.props.onSliderChanged(newValue)}
+        />
+        {currPageComponent}
+      </div>
+    )
+  }
 }
 
 SliderComponent.propTypes = {
