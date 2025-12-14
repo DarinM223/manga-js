2c5f22a17ddb5293174c8eef7215c563ce12e26c
d <darinm223@gmail.com>
2025-12-14 07:54:44 -0800

    Delete unnecessary files and modify readme

--- src/renderer/reducers/log.ts
+++ src/renderer/reducers/log.ts
@@ -4,7 +4,6 @@ import { produce } from 'immer'
 export type State = { [mangaName: string]: { [chapterNum: number]: boolean } }
 export const initState = {} as const satisfies State
 
-
 export function log(state: State = initState, action: Action): State {
   switch (action.type) {
     case SET_LOADING:
@@ -12,16 +11,6 @@ export function log(state: State = initState, action: Action): State {
         draft[action.mangaName] = draft[action.mangaName] ?? {}
         draft[action.mangaName][action.chapterNum] = true
       })
-    // return {
-    //   ...state,
-    //   [action.mangaName]: {
-    //     ...state[action.mangaName],
-    //     [action.chapterNum]: true
-    //   }
-    // }
-    // return produce(state, (draft) => {
-    //   draft[action.mangaName][action.chapterNum] = true
-    // })
     case LOAD_CHAPTER:
       return produce(state, (draft) => {
         delete draft[action.mangaName][action.chapterNum]
@@ -29,11 +18,6 @@ export function log(state: State = initState, action: Action): State {
           delete draft[action.mangaName]
         }
       })
-    // let newState = state.deleteIn([action.mangaName, action.chapterNum])
-    // if (newState.get(action.mangaName).count() <= 0) {
-    //   newState = newState.delete(action.mangaName)
-    // }
-    // return newState
     default:
       return state
   }
